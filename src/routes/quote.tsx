7811d3a4e5a509c93a4b988463b15826b6723c52
maintainance-marshall <smangalisonkosi96@gmail.com>
2026-08-03 11:55:51 +0200

    feat: add Hestiva privacy policy

--- src/routes/quote.tsx
+++ src/routes/quote.tsx
@@ -940,12 +940,8 @@ function Review({
         />
         <span>
           I consent to Hestiva contacting me about this enquiry.{" "}
-          <a
-            href="#privacy-notice"
-            onClick={(e) => e.preventDefault()}
-            className="font-semibold text-[#5A1425] underline underline-offset-4"
-          >
-            Privacy notice
+          <a href="/privacy" className="font-semibold text-[#5A1425] underline underline-offset-4">
+            Privacy Policy
           </a>
           .
         </span>
